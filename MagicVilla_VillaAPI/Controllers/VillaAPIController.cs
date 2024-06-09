55dcbd402b07b8855ee6965e4084d059a9c28784
Leonel <leonel48473@hotmail.es>
2024-06-08 21:20:12 -0400

    Add project files.

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,145 @@
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        private readonly ILogger<VillaAPIController> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public VillaAPIController(ILogger<VillaAPIController> logger, ApplicationDbContext db, IMapper mapper)
+        {
+            _logger = logger;
+            _db = db;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<VillaDTO>>> GetVillas()
+        {
+            IEnumerable<Villa> villaList = await _db.Villas.ToListAsync();
+            return Ok(_mapper.Map<List<VillaDTO>>(villaList));
+        }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<VillaDTO>> GetVillas(int id)
+        {
+            if (id == 0)
+            {
+                _logger.LogInformation("Get Villa Error with Id: " + id);
+                return BadRequest();
+            }
+            var villa = await _db.Villas.FirstOrDefaultAsync(x => x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<VillaDTO>(villa));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<VillaDTO>> CreateVilla([FromBody] VillaCreateDTO createvillaDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (await _db.Villas.FirstOrDefaultAsync(x => x.Name.ToLower() == createvillaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Esta villa ya existe!");
+                return BadRequest(ModelState);
+            }
+            if (createvillaDTO is null)
+            {
+                return BadRequest();
+            }
+            Villa model = _mapper.Map<Villa>(createvillaDTO);
+
+            await _db.Villas.AddAsync(model);
+            await _db.SaveChangesAsync();
+            return CreatedAtRoute("GetVilla", new { id = model.Id }, model);
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = await _db.Villas.FirstOrDefaultAsync(x => x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            _db.Villas.Remove(villa);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> UpdateVilla(int id, [FromBody] VillaUpdateDTO updateVillaDTO)
+        {
+            if (updateVillaDTO is null || id != updateVillaDTO.Id)
+            {
+                return BadRequest();
+            }
+            Villa model = _mapper.Map<Villa>(updateVillaDTO);
+
+            _db.Update(model);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> UpdatePartialVilla(int id, JsonPatchDocument<VillaUpdateDTO> patchDTO)
+        {
+            if (patchDTO is null || id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = await _db.Villas.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            VillaUpdateDTO villaDTO = _mapper.Map<VillaUpdateDTO>(villa);
+
+            if (villa is null)
+            {
+                return BadRequest();
+            }
+            patchDTO.ApplyTo(villaDTO, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Villa model = _mapper.Map<Villa>(villaDTO);
+
+            _db.Update(model);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,80 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                            new Villa
+                            {
+                                Id = 1,
+                                Name = "Villa AqzEF",
+                                Details = "Modern amenities and spacious rooms",
+                                Rate = 123.45,
+                                Sqft = 2500,
+                                Occupancy = 4,
+                                ImageUrl = "https://example.com/image1.jpg",
+                                Amenity = "Wi-Fi",
+                                CreatedDate = DateTime.Now,
+                            },
+            new Villa
+            {
+                Id = 2,
+                Name = "Villa GhuTR",
+                Details = "Beautiful view of the ocean",
+                Rate = 300.75,
+                Sqft = 3200,
+                Occupancy = 6,
+                ImageUrl = "https://example.com/image2.jpg",
+                Amenity = "Pool",
+                CreatedDate = DateTime.Now,
+            },
+            new Villa
+            {
+                Id = 3,
+                Name = "Villa WxzYH",
+                Details = "Private pool included",
+                Rate = 450.00,
+                Sqft = 4000,
+                Occupancy = 8,
+                ImageUrl = "https://example.com/image3.jpg",
+                Amenity = "Air Conditioning",
+                CreatedDate = DateTime.Now,
+            },
+            new Villa
+            {
+                Id = 4,
+                Name = "Villa OpqLM",
+                Details = "Close to city center",
+                Rate = 200.99,
+                Sqft = 1500,
+                Occupancy = 2,
+                ImageUrl = "https://example.com/image4.jpg",
+                Amenity = "Gym",
+                CreatedDate = DateTime.Now,
+            },
+            new Villa
+            {
+                Id = 5,
+                Name = "Villa VrjHK",
+                Details = "Pet-friendly accommodation",
+                Rate = 275.80,
+                Sqft = 1800,
+                Occupancy = 3,
+                ImageUrl = "https://example.com/image5.jpg",
+                Amenity = "Free Breakfast",
+                CreatedDate = DateTime.Now,
+            }
+                );
+        }
+    }
+}
